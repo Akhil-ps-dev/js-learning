e986b9d2f736ffc4e106bd15f35c2fd8d5a54e84
Chatter-Boo <psakhil211@gmail.com>
2022-07-19 18:52:27 +0530

    array

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-console.log('hello world');
-console.log(1234);
-console.log(null);
-console.log(undefined);
+// console.log('hello world');
+// console.log(1234);
+// console.log(null);
+// console.log(undefined);
 
 
-var age = 12;
+// var age = 12;
+// var boolean =true;
+// var decimal = 2.43;
+// console.log(decimal);
+// console.log(age);
 
-console.log(age);
+// console.log("Age:" ,age);
+// console.log("name:" ,boolean);
+
+var fullName= [
+    "John",
+    "Akhil",
+    "Deo",
+
+
+];
+
+console.log(fullName[2]);
+fullName.push("divya");
+console.log(fullName);
