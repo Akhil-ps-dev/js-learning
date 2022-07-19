9d2741da1b642e278ee0b0089d807a7065ea09bf
Chatter-Boo <psakhil211@gmail.com>
2022-07-19 19:38:49 +0530

    object

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 // console.log(null);
 // console.log(undefined);
 
-
 // var age = 12;
 // var boolean =true;
 // var decimal = 2.43;
@@ -13,13 +12,14 @@
 // console.log("Age:" ,age);
 // console.log("name:" ,boolean);
 
-var fullName= [
-    "John",
-    "Akhil",
-    "Deo",
+var fullName = ["John", "Akhil", "Deo"];
 
+// var something= [
+// "akhil",
+// 1234,
+// "asdasd"
 
-];
+// ];
 
 // console.log(fullName[2]);
 // fullName.push("divya");
@@ -27,11 +27,21 @@ var fullName= [
 // fullName.pop();
 // fullName.pop(0);
 // console.log(fullName);
+// map function
+// fullName.map((name)=>{
+// console.log(name);
 
-    fullName.map((name)=>{
-    console.log(name);
+// }
+// )
 
-
-
-    }
-    )
+//Objects
+var objects = {
+  age: 23,
+  string: "akhil",
+  name: "lemonade",
+  happy: true,
+  friends: ["akhil", "divya", "deo"],
+  
+};
+console.log(objects);
+console.log(objects.friends);
